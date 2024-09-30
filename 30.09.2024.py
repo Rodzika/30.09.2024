96480193d5307571bbc15f2aad1ed56943dc9a1b
Rodzika <Kendijarodzika20@gmail.com>
2024-09-30 13:13:37 +0300

    Add files via upload

---
+++ 30.09.2024.py
@@ -0,0 +1,15 @@
+a = 10 # a - int type variable
+b= 500
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+print(a ** b)
+print(a // b)
+
+user = input('Enter your name:')
+print(f"Hello {user}!")
+user2 = int(input('Enter number:'))
+
+
